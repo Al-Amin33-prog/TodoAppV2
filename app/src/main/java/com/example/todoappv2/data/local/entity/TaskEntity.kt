0bd704c9b4e4cf616b6be0c8102546e815ac6e9d
AL-Amin <aminushaba33@gmail.com>
2026-01-22 12:29:53 +0100

    Project & Ui Foundation                           init(ui): establish initial UI foundation      feat(ui): set up base theme and typography  feat(ui): add light and dark color schemes    chore(ui): apply theme to MainActivity and root composable                                    <->  Project Structure                          feat(ui): add reusable UI components           chore(project): define clean package structure for ui, features, data, and util     feat(util): add application constants and helper utilities                                      <-> Utilities                             feat(utilities): add application constants and utilities file                                   <-> Navigation                              feat(nav): added navigation file                <-> Data Layer                            data(core): add DAO for task and subjects   add entity for task and subjects

---
+++ app/src/main/java/com/example/todoappv2/data/local/entity/TaskEntity.kt
@@ -0,0 +1,32 @@
+package com.example.todoappv2.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    "tasks",
+    foreignKeys = [
+        ForeignKey(
+            entity = SubjectEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["subjectId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index("subjectId")]
+
+
+)
+data class TaskEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,
+    val subjectId: Long,
+    val title:String,
+    val description: String? =  null,
+    val dueDate: Long? = null,
+    val isCompleted: Boolean = false,
+    val createdAt: Long = System.currentTimeMillis()
+
+)
