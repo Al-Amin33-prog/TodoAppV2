b90baaf96eefc441a4b7900c6ce6bd1502719fc6
AL-Amin <aminushaba33@gmail.com>
2026-01-22 12:23:27 +0100

    Project & Ui Foundation                           init(ui): establish initial UI foundation      feat(ui): set up base theme and typography  feat(ui): add light and dark color schemes    chore(ui): apply theme to MainActivity and root composable                                    <->  Project Structure                          feat(ui): add reusable UI components           chore(project): define clean package structure for ui, features, data, and util     feat(util): add application constants and helper utilities

---
+++ app/src/main/java/com/example/todoappv2/data/local/entity/SubjectEntity.kt
@@ -0,0 +1,14 @@
+package com.example.todoappv2.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity("subjects")
+data class SubjectEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,
+    val name: String,
+    val colorHex: String,
+    val createdAt: Long =  System.currentTimeMillis()
+)
+
