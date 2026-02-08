a33b21e513239056c9c3066d7a689e55313672b1
AL-Amin <aminushaba33@gmail.com>
2026-02-08 16:59:00 +0100

    Project & Ui Foundation                        init(ui): establish initial UI foundation     feat(ui): set up base theme and typography  feat(ui): add light and dark color schemes    chore(ui): apply theme to MainActivity and root composable                                    <-> Project Structure  <->                feat(ui): add reusable UI components          chore(project): define clean package structure for ui, features, data, and util     feat(util): add application constants and helper utilities                                      <-> Utilities <->                     feat(utilities): add application constants and utilities file                                       <->Navigation<->                  feat(nav): added navigation file  refactor(fix): fixed bug on navigation routes  refactor(fix): removed unnecessary  routes            <-> Data Layer <->                      data(core): add DAO for task and subjects   add entity for task and subjects feat(data): align DAO queries with entity field names feat(data):configure Room database with Subject and Task Tables refactor(data): removed unused Dao update to simplify API                                   feat(data):configure firebase to project           <-> repository <-> refactor(repository): introduce AppRepository as single source of truth refactor(repository): delegate subject and task operations to DAO fix(repository): correct return types for subject and task flows refactor(repository): remove redundant repositories in favor of unified design refactor(repository): fixed some bug                 <->ViewModel<->  feat(subject): add SubjectViewModel with state and event handling  feat(subject):observe subjects using StateFlow  fix(subject): map UI state fields to subjectEntity correctly  feat(subject): add UI state holder for subject event  fix(subject): refactor fake app repository to match main repository
     refactor(subject): centralize subject actions
     using subject events
                 <-> NAVIGATION <->
     feat(nav):implemented nav routes
                <-> Auth <->
     feat(auth): implemented auth structure
                    <->UI layer<->
     feat(subject-ui): add subjectScreen with loading and empty states
     chore(subject-ui): add placeholder components for subject ui
     fix(subject-ui):correct event wiring between screen and viewModel
     refactor(topAppBar): added top app title
     refactor(HomeScreen): introduced HomeScreen compose ui setup
                                     <-> ViewModel<->                       feat(task): implement taskViewModel , and to also support filtering                      feat(subject): implement subjectViewModel
     refactor(task) : allow viewModel to get alarms when triggered by creating a task
     refactor(statistics): added stat screen
                    <-> ViewModelTest<->                 feat(subjectTest)introduce test for subject add feat(subjectTest)introduce test for subject delete
     feat(subjectTest): ran test on add and delete subjects                                      feat(taskTest): ran test on task items       feat(taskTest): introduce test on task update                                        feat(subjectTest): resolve a bug             feat(taskTest): ran test on task to update visible tasks                          feat():added auth teest for both log in, logout , and reset password
     feat(auth):ran integration test for auth
     feat(dao): refactor dao to fit in test
    
    Signed-off-by: AL-Amin <aminushaba33@gmail.com>

--- app/src/main/java/com/example/todoappv2/dashboard/DashBoardScreen00.kt
+++ app/src/main/java/com/example/todoappv2/dashboard/DashBoardScreen00.kt
@@ -1,2 +1,9 @@
 package com.example.todoappv2.dashboard
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(
+    navController: NavController
+){}
